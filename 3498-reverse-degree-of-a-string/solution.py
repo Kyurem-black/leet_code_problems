3a723cd84b5cff02aeb79e03d79775f7c6b206de
Kyurem-black <24a95a6110@aec.edu.in>
2026-09-20 09:55:13 +0530

    Reverse Degree of a String - 2026-09-20T04:25:12.991Z

--- 3498-reverse-degree-of-a-string/solution.py
+++ 3498-reverse-degree-of-a-string/solution.py
@@ -1,13 +1,23 @@
-// 7 ms | 19.2 MB
+// 7 ms | 19.5 MB
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        total = 0
-        idx = 1
-        for i in s:
-            value = 26 - (ord(i) - ord('a'))
-            total += idx * value
-            idx+=1
-        return total
+
+        h = {
+                'a': 26, 'b': 25, 'c': 24, 'd': 23, 'e': 22, 'f': 21,
+                'g': 20, 'h': 19, 'i': 18, 'j': 17, 'k': 16, 'l': 15,
+                'm': 14, 'n': 13, 'o': 12, 'p': 11, 'q': 10, 'r': 9,
+                's': 8, 't': 7, 'u': 6, 'v': 5, 'w': 4, 'x': 3, 'y': 2,
+                'z': 1
+            }
+
+        S=0
+
+        for i in range(len(s)):
+            S+=(h[s[i]]*(i+1))
+        return S
+
+        
+        
 
 
 
